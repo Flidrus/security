08d28ad64ce01dde285944e1ec7a2bdbea7b6a85
Flidrus <145051041+Flidrus@users.noreply.github.com>
2023-11-12 23:33:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lab5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            string userName;
+
+            while (true)
+            {
+                Console.Write("Do you want to log in (l) or sign up (s) (e to exit)?: ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine("\n\n");
+
+                switch (choice)
+                {
+                    case 'l':
+                    case 's':
+                        Console.Write("Enter your username: ");
+                        userName = Console.ReadLine();
+                        UserAuthentication.AuthenticateUser(userName, choice);
+                        break;
+                    case 'e':
+                        break;
+                    default:
+                        Console.WriteLine("Error! Unrecognized command");
+                        break;
+                }
+
+                if (choice == 'e') break;
+            }
+        }
+    }
+
+    internal static class UserAuthentication
+    {
+        public static void AuthenticateUser(string username, char action)
+        {
+            // Simulate user authentication logic
+            if (action == 'l')
+            {
+                Console.WriteLine($"Welcome back, {username}! You are now logged in.");
+            }
+            else if (action == 's')
+            {
+                Console.WriteLine($"Account created for {username}. You are now signed up.");
+            }
+        }
+    }
+}
