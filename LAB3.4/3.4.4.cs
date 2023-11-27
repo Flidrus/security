9161255fb23cd213cc51321a0ff9102d07d4489c
Flidrus <145051041+Flidrus@users.noreply.github.com>
2023-11-27 18:30:45 +0300

    Update 3.4.4.cs

--- LAB3.4/3.4.4.cs
+++ LAB3.4/3.4.4.cs
@@ -2,52 +2,56 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 
-class Program
+namespace Lz3_4_4
 {
-    static string CalculateHash(string input)
-    {
-        using (SHA256 sha256Hash = SHA256.Create())
-        {
-            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
 
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
+    class Program
+    {
+        static string CalculateHash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
             {
-                builder.Append(bytes[i].ToString("x2"));
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
             }
-            return builder.ToString();
         }
-    }
 
-    static bool VerifyPassword(string storedHash, string userInput)
-    {
-        string hashedInput = CalculateHash(userInput);
-        return storedHash.Equals(hashedInput, StringComparison.OrdinalIgnoreCase);
-    }
-
-    static void Main()
-    {
-        // Приклад реєстрації користувача
-        string username = "user123";
-        string password = "SecretPassword";
-
-        // Зберігаємо хеш пароля в базі даних (зазвичай вона буде представлена)
-        string storedPasswordHash = CalculateHash(password);
-
-        // Приклад авторизації
-        Console.WriteLine("Enter username:");
-        string inputUsername = Console.ReadLine();
-
-        Console.WriteLine("Enter password:");
-        string inputPassword = Console.ReadLine();
-
-        if (inputUsername == username && VerifyPassword(storedPasswordHash, inputPassword))
+        static bool VerifyPassword(string storedHash, string userInput)
         {
-            Console.WriteLine("Authentication successful!");
+            string hashedInput = CalculateHash(userInput);
+            return storedHash.Equals(hashedInput, StringComparison.OrdinalIgnoreCase);
         }
-        else
+
+        static void Main()
         {
-            Console.WriteLine("Authentication failed!");
+            // Приклад реєстрації користувача
+            string username = "user123";
+            string password = "SecretPassword";
+
+            // Зберігаємо хеш пароля в базі даних (зазвичай вона буде представлена)
+            string storedPasswordHash = CalculateHash(password);
+
+            // Приклад авторизації
+            Console.WriteLine("Enter username:");
+            string inputUsername = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string inputPassword = Console.ReadLine();
+
+            if (inputUsername == username && VerifyPassword(storedPasswordHash, inputPassword))
+            {
+                Console.WriteLine("Authentication successful!");
+            }
+            else
+            {
+                Console.WriteLine("Authentication failed!");
+            }
         }
     }
 }
